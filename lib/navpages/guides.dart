41149fca1ab9b997d88805175f58234d09f48a56
Zena Ataya <atayaz14@students.ecu.edu>
2023-03-22 17:22:11 -0400

    navpage folder
    adding the required folder for nav page

---
+++ lib/navpages/guides.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/cupertino.dart';
+
+class GuidesPage extends StatelessWidget {
+  const GuidesPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        child:Center(
+          child: Text(
+              "Guides Page"
+          ),
+        )
+    );
+  }
+}
---
+++ lib/navpages/homePage.dart
@@ -0,0 +1,105 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import '../widgets/appTextLarge.dart';
+
+class LandingPage extends StatefulWidget {
+  const LandingPage({Key? key}) : super(key: key);
+
+  @override
+  State<LandingPage> createState() => _LandingPageState();
+}
+
+class _LandingPageState extends State<LandingPage> with TickerProviderStateMixin{
+  @override
+  Widget build(BuildContext context) {
+    TabController _tabController = TabController(length: 3, vsync: this);
+    return Scaffold(
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            padding: const EdgeInsets.only(top:70, left: 20),
+            child: Row(
+              children: [
+                Icon(Icons.menu, size: 30, color: Colors.black54),
+                Expanded(child: Container()),
+                Container(
+                  margin: const EdgeInsets.only(right: 20),
+                  width: 50,
+                  height: 50,
+
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: Colors.grey.withOpacity(0.5),
+                  ),
+                )
+              ],
+            ),
+          ),//Container for hamburger & color icon
+          SizedBox(height: 40,),
+          Container(
+            margin: const EdgeInsets.only(left: 20),
+            child: LargeText(text: "Welcome"),
+          ),
+          SizedBox(height: 30,),
+          //tab
+          Container(
+            child: TabBar(
+                controller: _tabController,
+                labelColor: Colors.black,
+                unselectedLabelColor: Colors.grey,
+                isScrollable: true,
+                indicatorSize: TabBarIndicatorSize.label,
+                indicator: CircleTabIndicator(color: Colors.black54, radius: 4),
+                tabs: [
+                  Tab(text: "Home"),
+                  Tab(text:"Emergency Numbers"),
+                  Tab(text:"Backpack Essentials"),
+                ]
+            ),
+          ),
+          Container(
+              height: 300,
+              width: double.maxFinite,
+              child: TabBarView(
+                controller: _tabController,
+                children: [
+                  Text("Here will be a main page of featured how-to guides"),
+                  Text("Emergency phone numbers: (ex: Poison control, etc"),
+                  Text("Here will be a list of important items for to-go/bug-out bags")
+                ],
+              )
+          )
+        ],
+      ),
+    );
+  }
+}
+
+
+class CircleTabIndicator extends Decoration{
+  final Color color;
+  double radius;
+  CircleTabIndicator({required this.color, required this.radius});
+  @override
+  BoxPainter createBoxPainter([VoidCallback? onChanged]) {
+    // TODO: implement createBoxPainter
+    return _CirclePainter(color:color,radius:radius);
+  }
+}
+class _CirclePainter extends BoxPainter{
+  final Color color;
+  double radius;
+  _CirclePainter({required this.color, required this.radius});
+  @override
+  void paint(Canvas canvas, Offset offset, ImageConfiguration configuration) {
+    Paint _paint = Paint();
+    _paint.color = color;
+    _paint.isAntiAlias = true;
+    final Offset circleOffset = Offset(configuration.size!.width/2 - radius/2, configuration.size!.height - radius);
+    canvas.drawCircle(offset + circleOffset, radius, _paint);
+  }
+
+}
+
