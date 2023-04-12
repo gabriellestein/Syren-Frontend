752a4089afc9502208ae67d8ab80834718f1a8fc
Austin Finlayson <finlaysona20@students.ecu.edu>
2023-04-12 16:36:55 -0400

    Removed references to interface class in main, to ensure no errors caused on merge

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:syren_front_end/navpages/homePage.dart';
 import 'package:syren_front_end/navpages/searchPage.dart';
 import 'package:syren_front_end/navpages/splashPage.dart';
-import 'package:syren_front_end/backEndInterfaceScripts/Interface.dart';
+//import 'package:syren_front_end/backEndInterfaceScripts/Interface.dart';
 
 void main() {
   runApp(const MyApp());
-  Interface i = Interface();
 }
 
 class MyApp extends StatelessWidget {
